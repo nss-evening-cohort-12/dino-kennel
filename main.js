36f3a81c5b008ffa860d96b8749cca686ce03b6b
Luke Lancaster <luketlancaster@gmail.com>
2020-05-23 09:48:11 -0500

    create and test printtodom

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('Hello there 🦖');
+
+const printToDom = (selector, textToPrint) => {
+  document.querySelector(selector).innerHTML = textToPrint;
+}
+
+printToDom('#main', '<h2><i class="fas fa-cat"></i></h2>')
